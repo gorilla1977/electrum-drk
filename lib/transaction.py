21899aef601619b179d51e30efdd80203e577831
Andriy Khavryuchenko <akhavr@khavr.com>
2014-11-09 19:59:15 +0200

    ticket:1: changed addrtype to use keys for darkcoin testnet

--- lib/transaction.py
+++ lib/transaction.py
@@ -576,7 +576,7 @@ class Transaction:
         else:
             assert type == 'address'
         addrtype, hash_160 = bc_address_to_hash_160(addr)
-        if addrtype == 0:
+        if addrtype == 111:
             script = '76a9'                                      # op_dup, op_hash_160
             script += push_script(hash_160.encode('hex'))
             script += '88ac'                                     # op_equalverify, op_checksig
